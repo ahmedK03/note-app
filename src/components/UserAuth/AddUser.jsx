12de7f1a76c9611dc2216525903744847da15589
ahmedK03 <ahmed.khaled.fawzy@hotmail.com>
2022-02-18 07:16:57 +0200

    validating registeration via useReducer

--- src/components/UserAuth/AddUser.jsx
+++ src/components/UserAuth/AddUser.jsx
@@ -1,179 +1,175 @@
-import useInput from '../../hooks/use-input'; // custom hook
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import axios from 'axios';
-import './AddUser.css';
+import useInput from "../../hooks/use-input"; // custom hook
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import axios from "axios";
+import "./AddUser.css";
 
 const endPoint = process.env.REACT_APP_ROUTE_EGY_ENDPOINT;
 
-/*
-const emailReducer = ( oldState, action ) => {
-    switch (action.type) {
-        case 'EMAIL_INPUT_CHANGE' :
-            return { value: action.value, isValid: action.value.includes('@') };
-        case 'EMAIL_INPUT_BLUR' :
-            return { value:oldState.value, isValid: oldState.value.includes('@')};
-        default: 
-            return {value: '', isValid: false};
-    }
-}
+const nameNotValid = (value) => value.trim().length >= 3;
+const emailNotVaild = (value) => value.includes("@");
+const passwordNotValid = (value) => value.trim().length > 8;
 
-const passwordReducer = ( oldState, action ) => {
+let formIsValid = false;
 
-}
-*/
 const AddUser = () => {
+  const {
+    value: enteredFname,
+    isValid: enteredFnameIsValid,
+    hasError: fNameHasError,
+    valueChangeHandler: fNameChangeHandler,
+    inputBlurHandler: fNameBlurHandler,
+    reset: resetFname,
+    isInValid: fNameisInValid,
+  } = useInput(nameNotValid);
 
-    /*
+  const {
+    value: enteredLname,
+    isValid: enteredLnameIsValid,
+    hasError: lNameHasError,
+    valueChangeHandler: lNameChangeHandler,
+    inputBlurHandler: lNameBlurHandler,
+    reset: resetLname,
+    isInValid: lNameisInValid,
+  } = useInput(nameNotValid);
 
-    const [emailState, emailDispatch] = useReducer(emailReducer, {value:'',isValid:''});
-    const [passwordState, passwordDispatch] = useReducer(passwordReducer, {value:'', isValid:''});
-    const userFirstName = useRef();
-    const userLastName = useRef();
-    const userEmail = useRef();
-    const userPassword = useRef();
-    
-    
-    const emailChangeHandler = ({ target }) => {
-        emailDispatch({type:'EMAIL_INPUT_CHANGE', value:target.value});
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: emailHasError,
+    valueChangeHandler: emailInputChangeHandler,
+    inputBlurHandler: emailInputBlurHandler,
+    reset: resetEmail,
+    isInValid: emailisInValid,
+  } = useInput(emailNotVaild);
+
+  const {
+    value: enteredPassword,
+    isValid: enteredPasswordIsValid,
+    hasError: passwordHasError,
+    valueChangeHandler: passwordInputChangeHandler,
+    inputBlurHandler: passowrdInputBlutHandler,
+    reset: resetPassword,
+    inInValid: passwordisInValid,
+  } = useInput(passwordNotValid);
+
+  const valuesAreValid =
+    enteredFnameIsValid &&
+    enteredLnameIsValid &&
+    enteredEmailIsValid &&
+    enteredPasswordIsValid;
+
+  const valuesAreinValid =
+    !enteredEmailIsValid ||
+    !enteredFnameIsValid ||
+    !enteredLnameIsValid ||
+    !enteredPasswordIsValid;
+
+  if (valuesAreValid) {
+    formIsValid = true;
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (valuesAreinValid) {
+      return;
     }
 
-    const validateEmailHandler = () => {
-        emailDispatch({type:'EMAIL_INPUT_BLUR'})
-    }
-    */
+    const userInfo = {
+      first_name: enteredFname,
+      last_name: enteredLname,
+      email: enteredEmail,
+      password: enteredPassword,
+    };
+    console.log(userInfo);
 
-    const {
-        value: enteredFname, 
-        isValid: enteredFnameIsValid,
-        hasError: fNameHasError,
-        valueChangeHandler: fNameChangeHandler,
-        inputBlurHandler: fNameBlurHandler,
-        reset: resetFname,
-        isInValid: fNameisInValid
-    } = useInput((value) => value.trim().length >= 3);
+    resetFname();
+    resetLname();
+    resetEmail();
+    resetPassword();
+  };
 
-    const {
-        value:enteredLname,
-        isValid: enteredLnameIsValid,
-        hasError: lNameHasError,
-        valueChangeHandler: lNameChangeHandler,
-        inputBlurHandler: lNameBlurHandler,
-        reset: resetLname,
-        isInValid: lNameisInValid
-    } = useInput((value) => value.trim().length >= 3)
+  return (
+    <section
+      id="registerForm"
+      className="d-flex justify-content-center align-items-center">
+      <Container>
+        <Col md={6} className="mx-auto">
+          <Form onSubmit={submitHandler}>
+            <Form.Group className="mb-3" controlId="userFirstName">
+              <Form.Label>Enter your first name</Form.Label>
+              <Form.Control
+                type="text"
+                onChange={fNameChangeHandler}
+                onBlur={fNameBlurHandler}
+                value={enteredFname}
+                className={fNameisInValid}
+                placeholder="First name"
+                name="first_name"
+              />
+              {fNameHasError && (
+                <p className="text-danger">Name must be >= 3 letters</p>
+              )}
+            </Form.Group>
 
-    const {
-        value:enteredEmail,
-        isValid: enteredEmailIsValid,
-        hasError: emailHasError,
-        valueChangeHandler: emailInputChangeHandler,
-        inputBlurHandler: emailInputBlurHandler,
-        reset: resetEmail,
-        isInValid: emailisInValid
-    } = useInput( (value) => value.includes('@'))
+            <Form.Group className="mb-3" controlId="userLastName">
+              <Form.Label>Enter your last name</Form.Label>
+              <Form.Control
+                type="text"
+                onChange={lNameChangeHandler}
+                onBlur={lNameBlurHandler}
+                value={enteredLname}
+                className={lNameisInValid}
+                name="last_name"
+                placeholder="Last Name"
+              />
+              {lNameHasError && (
+                <p className="text-danger">Name must be >= 3 letters</p>
+              )}
+            </Form.Group>
 
-    let formIsValid = false;
+            <Form.Group className="mb-3" controlId="userEmail1">
+              <Form.Label>Enter Your Email Address</Form.Label>
+              <Form.Control
+                name="email"
+                value={enteredEmail}
+                className={emailisInValid}
+                onChange={emailInputChangeHandler}
+                onBlur={emailInputBlurHandler}
+                placeholder="example@email.net"
+              />
+              {emailHasError && <p className="text-danger">Email is invalid</p>}
+            </Form.Group>
 
-    if(enteredFnameIsValid && enteredLnameIsValid && enteredEmailIsValid) {
-        formIsValid = true;
-    }
+            <Form.Group className="mb-3" controlId="userPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                name="password"
+                value={enteredPassword}
+                className={passwordisInValid}
+                onChange={passwordInputChangeHandler}
+                onBlur={passowrdInputBlutHandler}
+                type="password"
+                name="password"
+              />
+              {passwordHasError && (
+                <p className="text-danger">
+                  Password must be more than 8 Characters
+                </p>
+              )}
+            </Form.Group>
 
-    const submitHandler =  (e) => {
-        e.preventDefault();
-        
-        if(!enteredFnameIsValid || !enteredLnameIsValid || !enteredEmailIsValid ) {
-            return;
-        }
-
-        const userInfo = {
-            first_name: enteredFname,
-            last_name: enteredLname,
-            email: enteredEmail,
-            password: ''
-        }
-        console.log(userInfo);
-
-
-        resetFname();
-        resetLname();
-        resetEmail();
-        /*
-        const userInfo = {
-            first_name: userFirstName.current.value,
-            last_name: userLastName.current.value,
-            email: userEmail.current.value,
-            password: userPassword.current.value
-        }
-        let { data } = await axios.post(`${endPoint}/signup`,userInfo );
-        */
-    }
-
-    
-
-    return(
-        <section id="registerForm" className="d-flex justify-content-center align-items-center">
-            <Container>
-                    <Col md={6} className="mx-auto">
-                        <Form onSubmit={submitHandler}>
-
-                            <Form.Group className="mb-3" controlId="userFirstName">
-                                <Form.Label>Enter your first name</Form.Label>
-                                <Form.Control 
-                                type="text"
-                                onChange={fNameChangeHandler}
-                                onBlur={fNameBlurHandler}
-                                value={enteredFname}
-                                className={fNameisInValid}
-                                placeholder="First name" 
-                                name="first_name"/>
-                                {fNameHasError && <p className="text-danger">Name must be >= 3 letters</p>}
-                            </Form.Group>
-
-                            <Form.Group className="mb-3" controlId="userLastName">
-                                <Form.Label>Enter your last name</Form.Label>
-                                <Form.Control 
-                                type="text"
-                                onChange={lNameChangeHandler}
-                                onBlur={lNameBlurHandler}
-                                value={enteredLname}
-                                className={lNameisInValid}
-                                name="last_name"
-                                placeholder="Last Name" />
-                                {lNameHasError && <p className="text-danger">Name must be >= 3 letters</p>}
-                            </Form.Group>
-
-                            <Form.Group className="mb-3" controlId="userEmail1">
-                                <Form.Label>Enter Your Email Address</Form.Label>
-                                <Form.Control 
-                                name="email"
-                                value={enteredEmail}
-                                className={emailisInValid}
-                                onChange={emailInputChangeHandler}
-                                onBlur={emailInputBlurHandler}
-                                placeholder="example@email.net"
-                                />
-                                {emailHasError && <p className="text-danger">Email is invalid</p>}
-                            </Form.Group>
-
-                            <Form.Group className="mb-3" controlId="userPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control
-                                
-                                type="password"
-                                name="password"/>
-                            </Form.Group>
-
-                            <Button type="submit" disabled={!formIsValid} variant="primary">
-                                Submit
-                            </Button>
-                        </Form>
-                    </Col>
-            </Container>
-        </section>
-    );
-}
+            <Button type="submit" disabled={!formIsValid} variant="primary">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Container>
+    </section>
+  );
+};
 
 export default AddUser;
