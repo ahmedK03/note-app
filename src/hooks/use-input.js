44c3b345e88f6668bbb06527164dac08cb6831a2
ahmedK03 <ahmed.khaled.fawzy@hotmail.com>
2022-02-18 07:18:19 +0200

    customHook using uerReducer

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,39 +1,64 @@
-import { useState } from 'react';
+import { useReducer } from "react";
 
+const initialInputState = {
+  value: "",
+  isTouched: false,
+};
+
+const ACTIONS = {
+  INPUT_CHANGE_HANDLER: "change_handler",
+  INPUT_BLUR_HANDLER: "blur_handler",
+  INPUT_RESET: "reset_handler",
+};
+
+const inputStateReducer = (oldState, action) => {
+  switch (action.type) {
+    case ACTIONS.INPUT_CHANGE_HANDLER:
+      return { value: action.value, isTouched: oldState.isTouched };
+    case ACTIONS.INPUT_BLUR_HANDLER:
+      return { isTouched: true, value: oldState.value };
+    case ACTIONS.INPUT_RESET:
+      return { value: "", isTouched: false };
+    default:
+      return initialInputState;
+  }
+};
 
 const useInput = (validation) => {
+  const [inputState, inputDispatch] = useReducer(
+    inputStateReducer,
+    initialInputState
+  );
 
-    const [ enteredValue, setEnteredValue ] = useState('');
-    const [ inputIsTouched, setInputIsTouched ] = useState(false);
+  const valueIsValid = validation(inputState.value);
+  const hasError = !valueIsValid && inputState.isTouched;
 
-    const valueIsValid = validation(enteredValue);
-    const hasError = !valueIsValid && inputIsTouched;
+  const isInValid = hasError ? "is-invalid" : "";
 
-    const isInValid = hasError ? 'is-invalid' : '';
+  const valueChangeHandler = (e) => {
+    inputDispatch({
+      type: ACTIONS.INPUT_CHANGE_HANDLER,
+      value: e.target.value,
+    });
+  };
 
-    const valueChangeHandler = (e) => {
-        setEnteredValue(e.target.value);
-    }
+  const inputBlurHandler = () => {
+    inputDispatch({ type: ACTIONS.INPUT_BLUR_HANDLER });
+  };
 
-    const inputBlurHandler = () => {
-        setInputIsTouched(true);
-    }
+  const reset = () => {
+    inputDispatch({ type: ACTIONS.INPUT_RESET });
+  };
 
-    const reset = ( ) => {
-        setEnteredValue('');
-        setInputIsTouched(false);
-    }
-
-
-    return {
-        value: enteredValue,
-        isValid: valueIsValid,
-        hasError,
-        isInValid,
-        valueChangeHandler,
-        inputBlurHandler,
-        reset
-    }
-}
+  return {
+    value: inputState.value,
+    isValid: valueIsValid,
+    hasError,
+    isInValid,
+    valueChangeHandler,
+    inputBlurHandler,
+    reset,
+  };
+};
 
 export default useInput;
